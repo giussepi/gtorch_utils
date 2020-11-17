31dc116503f8d9aca966985e295867be6ede7c20
giussepi <giussepexy@gmail.com>
2020-11-17 18:05:26 +0000

    gtorch_utils/datasets/generic-> BaseDataset providing example code

--- gtorch_utils/datasets/generic.py
+++ gtorch_utils/datasets/generic.py
@@ -4,6 +4,8 @@
 from torch.utils.data import Dataset
 
 from gtorch_utils.constants import DB
+# from gutils.numpy_.numpy_ import LabelMatrixManager
+# from sklearn.model_selection import train_test_split
 
 
 class BaseDataset(Dataset):
@@ -22,6 +24,34 @@ class BaseDataset(Dataset):
         Operations required to enable the retrieval of feats and labels for the
         subset (train, val, or test)
         """
+        # e.g. ################################################################
+        # dbhandler = kwargs.get('dbhandler')
+        # normalizer = kwargs.get('normalizer')
+        # val_size = kwargs.get('val_size')
+
+        # if self.subset in (DB.TRAIN, DB.VALIDATION):
+        #     self.feats, self.labels = dbhandler(normalizer)()[:2]
+        #     self.labels = LabelMatrixManager.get_1d_array_from_2d_matrix(self.labels)
+
+        #     if subset == DB.TRAIN:
+        #         self.feats, _, self.labels, _ = train_test_split(
+        #             self.feats.T, self.labels, test_size=val_size, random_state=self.random_state,
+        #             stratify=self.labels
+        #         )
+        #     else:
+        #         _, self.feats, _,  self.labels = train_test_split(
+        #             self.feats.T, self.labels, test_size=val_size, random_state=self.random_state,
+        #             stratify=self.labels
+        #         )
+
+        #     self.feats = self.feats.T
+
+        # else:
+        #     self.feats, self.labels = dbhandler(normalizer)()[2:]
+        #     self.labels = LabelMatrixManager.get_1d_array_from_2d_matrix(self.labels)
+
+        # self.feats = self.feats.astype(np.float32)
+
         raise NotImplementedError
 
     def __len__(self):
@@ -29,6 +59,9 @@ class BaseDataset(Dataset):
         Returns:
             dataset size (int)
         """
+        # e.g. ################################################################
+        # return self.labels.shape[0]
+
         raise NotImplementedError
 
     def __getitem__(self, idx):
@@ -36,4 +69,17 @@ class BaseDataset(Dataset):
         Returns:
             dict(feats=..., label=...)
         """
+        # e.g. ################################################################
+        # if torch.is_tensor(idx):
+        #     idx = idx.tolist()
+
+        # # feats = self.get_default_data_transforms()[self.subset](self.feats[:, idx].ravel())
+        # # feats = self.get_default_data_transforms()[self.subset](
+        # #     self.feats[:, idx].ravel()[np.newaxis, np.newaxis, :])
+
+        # return dict(
+        #     feats=torch.from_numpy(self.feats[:, idx].ravel()),
+        #     label=self.labels[idx]
+        # )
+
         raise NotImplementedError
