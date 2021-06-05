9a75bcaadbe902707835942209d5db41b87dfbec
giussepi <giussepexy@gmail.com>
2021-06-05 12:43:03 +0100

    gtorch_utils/segmentation/metrics/rnpv.py RNPV

---
+++ gtorch_utils/segmentation/metrics/rnpv.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+""" gtorch_utils/segmentation/metrics/rnpv """
+
+import torch
+
+from gtorch_utils.segmentation.metrics import recall, npv
+
+
+class RNPV(torch.nn.Module):
+    """
+    Calculates and returns the Recall Negative Predictive Value (RNPV) score
+
+    RNPV = \frac{xi*recall + tau*npv}{xi + tau}
+
+    Usage:
+        score = RNPV()(predicted_masks, ground_truth_masks)
+    """
+
+    def __init__(self, xi=1, tau=1):
+        """
+        Initializes the object instance.
+
+        Args:
+            xi           <int, float>: recall weight. Default 1
+            tau          <int, float>: npv multiplier. Default 1
+        """
+        super().__init__()
+        assert isinstance(xi, (int, float)), type(xi)
+        assert isinstance(tau, (int, float)), type(tau)
+        assert xi > 0, xi
+        assert tau > 0, tau
+
+        self.xi = xi
+        self.tau = tau
+
+    def forward(self, preds, targets):
+        """
+        Calculates and returns the RNPV score
+
+        Args:
+            preds  <torch.Tensor>: predicted masks [batch_size, channels, ...]
+            target <torch.Tensor>: ground truth masks [batch_size, channels, ...]
+
+        Returns:
+            score <torch.Tensor>
+        """
+        assert isinstance(preds, torch.Tensor), type(preds)
+        assert isinstance(targets, torch.Tensor), type(targets)
+
+        return (self.xi * recall(preds, targets) + self.tau * npv(preds, targets)) / (self.xi + self.tau)
---
+++ gtorch_utils/segmentation/metrics/test/test_rnpv.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+""" gtorch_utils/segmentation/metrics/test/test_rnpv """
+
+import unittest
+
+import torch
+
+from gtorch_utils.segmentation.metrics import recall, npv, RNPV
+
+
+class Test_rnpv(unittest.TestCase):
+
+    def setUp(self):
+        self.pred = torch.Tensor([
+            [[1., 0., 0., 1., 0., 0., 0.], [1., 0., 0., 1., 1., 1., 0.]],
+            [[0., 1., 1., 1., 0., 1., 0.], [0., 0., 0., 0., 1., 1., 0.]]
+        ])
+        self.gt = torch.Tensor([
+            [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]],
+            [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]]
+        ])
+        self.tn = torch.Tensor([[2, 0], [1, 1]])
+        self.fn = torch.Tensor([[3, 3], [2, 4]])
+        self.tp = torch.Tensor([[1, 1], [2, 0]])
+
+    def test_function(self):
+        xi = 1.5
+        tau = 1.8
+        self.assertTrue(
+            RNPV(xi, tau)(self.pred, self.gt),
+            (xi * recall(self.pred, self.gt) + tau * npv(self.pred, self.gt)) / (xi + tau)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
