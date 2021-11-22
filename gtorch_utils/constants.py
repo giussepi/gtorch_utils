dddabec61c590160324d7feebc5da8ed80092f7e
giussepi <giussepexy@gmail.com>
2021-11-22 15:32:24 +0000

    gtorch_utils/constants->DB method to validate subdataset name

--- gtorch_utils/constants.py
+++ gtorch_utils/constants.py
@@ -12,3 +12,13 @@ class DB:
     TEST = 'test'
 
     SUB_DATASETS = [TRAIN, TEST, VALIDATION]
+
+    @classmethod
+    def clean_subdataset_name(cls, subdatset_name):
+        """
+        Validates the subdataset name
+
+        Args:
+            db_split_name <str>: sub-dataset split name
+        """
+        assert subdatset_name in cls.SUB_DATASETS, f'{subdatset_name} is not in DB.SUB_DATASETS'
