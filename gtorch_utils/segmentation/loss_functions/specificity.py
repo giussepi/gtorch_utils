2cf66d1faece21c07e9a9d50694400249fdf0405
giussepi <giussepexy@gmail.com>
2022-02-11 15:20:35 +0000

    gtorch_utils/segmentation/loss_functions/specificity.py - > SpecificityLoss updated to receive logits_transform argument

--- gtorch_utils/segmentation/loss_functions/specificity.py
+++ gtorch_utils/segmentation/loss_functions/specificity.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """ gtorch_utils/segmentation/loss_functions/specificity """
 
+from typing import Callable
+
 import torch
 
 from gtorch_utils.segmentation.metrics import Specificity
@@ -17,22 +19,29 @@ class SpecificityLoss(torch.nn.Module):
         Specificity()(predictions, ground_truth)
     """
 
-    def __init__(self, *, with_logits=False, per_class=False):
+    def __init__(
+            self, *, with_logits: bool = False, per_class: bool = False,
+            logits_transform: Callable[[torch.Tensor], torch.Tensor] = torch.sigmoid
+    ):
         """
         Initializes the object instance
 
-        with_logits <bool>: set to True when working with logits to apply sigmoid
-        per_class <bool>: Set it to True to calculate specificity values per class
+        with_logits          <bool>: set to True when working with logits to apply sigmoid
+        per_class            <bool>: Set it to True to calculate specificity values per class
+        logits_transform <callable>: function to be applied to the logits in preds. Default torch.sigmoid
         """
         super().__init__()
         assert isinstance(with_logits, bool), type(with_logits)
         assert isinstance(per_class, bool), type(per_class)
+        assert callable(logits_transform), f'{logits_transform} must be a callable'
 
         self.with_logits = with_logits
         self.per_class = per_class
-        self.specificity = Specificity(with_logits=self.with_logits, per_class=self.per_class)
+        self.logits_transform = logits_transform
+        self.specificity = Specificity(
+            per_class=self.per_class, with_logits=self.with_logits, logits_transform=self.logits_transform)
 
-    def forward(self, preds, targets):
+    def forward(self, preds: torch.Tensor, targets: torch.Tensor):
         """
         Calculates and returns the specificity loss (true negative rate)
 
