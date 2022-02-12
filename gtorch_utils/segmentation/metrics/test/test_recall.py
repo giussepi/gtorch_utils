266d3701283ab495290cde07b301a2b9c887b41e
giussepi <giussepexy@gmail.com>
2022-02-12 22:46:39 +0000

    gtorch_utils/segmentation/metrics/test/test_recall.py tests updated

--- gtorch_utils/segmentation/metrics/test/test_recall.py
+++ gtorch_utils/segmentation/metrics/test/test_recall.py
@@ -11,36 +11,36 @@ from gtorch_utils.segmentation.metrics.recall import recall
 
 class Test_recall(unittest.TestCase):
 
-    def test_per_class_False(self):
-        pred = torch.Tensor([[[1., 0., 0., 1., 0., 0., 0.]]])
-        gt = torch.Tensor([[[1., 1., 1., 0., 0., 0., 1.]]])
-        tp = 1
-        fn = 3
-
-        self.assertEqual(
-            round(recall(pred, gt).item(), 2),
-            round(tp/(tp+fn+EPSILON), 2)
-        )
-
-    def test_per_class_True(self):
-        pred = torch.Tensor([
+    def setUp(self):
+        self.pred = torch.Tensor([
+            [[1., 0., 0., 1., 0., 0., 0.], [1., 0., 0., 1., 1., 1., 0.]],
+            [[1., 0., 0., 1., 0., 0., 0.], [1., 0., 0., 1., 1., 1., 0.]],
             [[1., 0., 0., 1., 0., 0., 0.], [1., 0., 0., 1., 1., 1., 0.]],
             [[0., 1., 1., 1., 0., 1., 0.], [0., 0., 0., 0., 1., 1., 0.]]
         ])
-        gt = torch.Tensor([
+        self.gt = torch.Tensor([
+            [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]],
+            [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]],
             [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]],
             [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]]
         ])
-        tp = torch.Tensor([[1, 1], [2, 0]])
-        fn = torch.Tensor([[3, 3], [2, 4]])
+        self.tp = torch.Tensor([[1, 1], [1, 1], [1, 1], [2, 0]])
+        self.fn = torch.Tensor([[3, 3], [3, 3], [3, 3], [2, 4]])
 
+    def test_per_class_False(self):
+        self.assertTrue(torch.equal(
+            recall(self.pred, self.gt),
+            self.tp.sum()/(self.tp.sum()+self.fn.sum()+EPSILON)
+        ))
+
+    def test_per_class_True(self):
         self.assertFalse(torch.equal(
-            recall(pred, gt, True),
-            tp/(tp+fn+EPSILON)
+            recall(self.pred, self.gt, True),
+            self.tp/(self.tp+self.fn+EPSILON)
         ))
         self.assertTrue(torch.equal(
-            recall(pred, gt, True),
-            tp.sum(0)/(tp.sum(0)+fn.sum(0)+EPSILON)
+            recall(self.pred, self.gt, True),
+            self.tp.sum(0)/(self.tp.sum(0)+self.fn.sum(0)+EPSILON)
         ))
 
 
