94bc1262d408f064b4e93e04689ad601536ceaee
giussepi <giussepexy@gmail.com>
2022-02-28 18:01:10 +0000

    gtorch_utils/segmentation/torchmetrics/test/test_accuracy.py -> Test_Accuracy removing duplicated setUp method

--- gtorch_utils/segmentation/torchmetrics/test/test_accuracy.py
+++ gtorch_utils/segmentation/torchmetrics/test/test_accuracy.py
@@ -11,24 +11,6 @@ from gtorch_utils.segmentation.torchmetrics.test.mixins import BaseSegmentationM
 
 class Test_Accuracy(BaseSegmentationMixin, unittest.TestCase):
 
-    def setUp(self):
-        self.pred = torch.Tensor([
-            [[1., 0., 0., 1., 0., 0., 0.], [1., 0., 0., 1., 1., 1., 0.]],
-            [[1., 0., 0., 1., 0., 0., 0.], [1., 0., 0., 1., 1., 1., 0.]],
-            [[1., 0., 0., 1., 0., 0., 0.], [1., 0., 0., 1., 1., 1., 0.]],
-            [[0., 1., 1., 1., 0., 1., 0.], [0., 0., 0., 0., 1., 1., 0.]]
-        ])
-        self.gt = torch.Tensor([
-            [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]],
-            [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]],
-            [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]],
-            [[1., 1., 1., 0., 0., 0., 1.], [1., 1., 1., 0., 0., 0., 1.]]
-        ])
-        self.tp = torch.Tensor([[1, 1], [1, 1], [1, 1], [2, 0]])
-        self.fp = torch.Tensor([[1, 3], [1, 3], [1, 3], [2, 2]])
-        self.tn = torch.Tensor([[2, 0], [2, 0], [2, 0], [1, 1]])
-        self.fn = torch.Tensor([[3, 3], [3, 3], [3, 3], [2, 4]])
-
     def test_per_class_False(self):
         self.assertTrue(torch.equal(
             Accuracy()(self.pred, self.gt),
